b07c1d463f7ecce47540bba0d6ee45390f33eb3b
Lukas Elmer <lukas.elmer@renuo.ch>
2016-11-26 17:12:24 +0100

    Add simple operation

--- playground.rb
+++ playground.rb
@@ -1,2 +1,16 @@
 require 'trailblazer/operation'
 
+class Create < Trailblazer::Operation
+  self.> :hello!
+  self.> :ciao!
+
+  def hello!(*)
+    p 'hello'
+  end
+
+  def ciao!(*)
+    p 'ciao'
+  end
+end
+
+Create.()
